fd849d58b896b7881eab95f9bfccc91d4c0dfe7e
DeepL1x <78798664+DeepL1x@users.noreply.github.com>
2023-12-06 15:04:19 +0200

    feat(Server): add basic server template

---
+++ src/Server.java
@@ -0,0 +1,80 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private static Boolean isIndexReady = false;
+
+    public void start(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                (new Thread(new ClientHandler(clientSocket))).start();
+                System.out.println("New client " + clientSocket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+                DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                String inputLine;
+                String[] words;
+                boolean shutdown = false;
+                String options = """
+                        Your options:
+                        1. Provide words to find
+                        2. Check index status
+                        3. Disconnect""";
+                dos.writeUTF(options);
+                while (!shutdown) {
+                    inputLine = dis.readUTF();
+                    switch (inputLine) {
+                        case "1" -> {
+                            dos.writeUTF("Enter separated by space words to find:");
+                            words = dis.readUTF().split(" ");
+                            dos.writeUTF("Result: " + String.join(" ", words));
+                        }
+                        case "2" -> {
+                            dos.writeUTF(isIndexReady ? "Index is populated and ready for use." : "Index is empty.");
+                        }
+                        case "3" -> {
+                            dos.writeUTF("Disconnected.");
+                        }
+                        default -> dos.writeUTF("Unknown option");
+                    }
+                }
+                dis.close();
+                dos.close();
+                clientSocket.close();
+                System.out.println("Client finished " + clientSocket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
